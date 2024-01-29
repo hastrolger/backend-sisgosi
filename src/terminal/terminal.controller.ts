004039774a106b17c8ab04960a09ee977171ba3a
hastrolger <parralesjael@gmail.com>
2024-01-29 14:09:32 -0800

    Adding terminal controller

--- src/terminal/terminal.controller.ts
+++ src/terminal/terminal.controller.ts
@@ -3,7 +3,7 @@ import { TerminalService } from './terminal.service';
 import { CreateTerminalDto } from './dto/create-terminal.dto';
 import { UpdateTerminalDto } from './dto/update-terminal.dto';
 
-@Controller('terminal')
+@Controller('terminals')
 export class TerminalController {
   constructor(private readonly terminalService: TerminalService) {}
 
